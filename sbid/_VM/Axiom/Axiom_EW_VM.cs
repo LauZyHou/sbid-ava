d0ac598fa7862306e1b17415e2a628567f9987ce
unknown <java233@foxmail.com>
2020-02-09 23:01:09 +0800

    Axiom的编辑实现

---
+++ sbid/_VM/Axiom/Axiom_EW_VM.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI;
+using sbid._M;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace sbid._VM
+{
+    public class Axiom_EW_VM : ViewModelBase
+    {
+        private Axiom axiom;
+        private ObservableCollection<_M.Type> types = new ObservableCollection<_M.Type>();
+        private ObservableCollection<Attribute> @params = new ObservableCollection<Attribute>();
+
+        // 要编辑的公理
+        public Axiom Axiom { get => axiom; set => axiom = value; }
+        // 所有Type对象(包括int, bool),作为Attribute/Method/CommMethod的可用类型
+        public ObservableCollection<sbid._M.Type> Types { get => types; set => types = value; }
+        // Method参数列表绑定此处
+        public ObservableCollection<Attribute> Params { get => @params; set => this.RaiseAndSetIfChanged(ref @params, value); }
+    }
+}
--- sbid/_VM/Axiom/Axiom_VM.cs
+++ sbid/_VM/Axiom/Axiom_VM.cs
@@ -1,4 +1,5 @@
 ﻿using sbid._M;
+using sbid._V;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,5 +18,33 @@ namespace sbid._VM
         }
 
         public Axiom Axiom { get => axiom; set => axiom = value; }
+
+        #region 右键菜单命令
+
+        // 尝试打开当前Axiom_VM的编辑窗体
+        public void EditAxiomVM()
+        {
+            // 从主窗体打开编辑窗体,并在其DataContext中集成当前Axiom_VM里集成的Axiom对象,以能对其作修改
+            Axiom_EW_V axiomEWV = new Axiom_EW_V()
+            {
+                DataContext = new Axiom_EW_VM()
+                {
+                    Axiom = axiom
+                }
+            };
+            // 将所有的Type也传入,作为Method的可用类型
+            foreach (NetworkItem_VM item in ResourceManager.mainWindowVM.SelectedItem.SelectedItem.SelectedItem.NetworkItemVMs)
+            {
+                if (item is UserType_VM)
+                {
+                    ((Axiom_EW_VM)axiomEWV.DataContext).Types.Add(((UserType_VM)item).Type);
+                }
+            }
+
+            axiomEWV.ShowDialog(ResourceManager.mainWindowV);
+            ResourceManager.mainWindowVM.Tips = "打开了公理：" + axiom.Name + "的编辑窗体";
+        }
+
+        #endregion
     }
 }
