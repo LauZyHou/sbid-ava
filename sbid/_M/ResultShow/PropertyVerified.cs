e622691a8039b77a3c7e77febf6d0a26bb948f29
unknown <java233@foxmail.com>
2020-08-04 23:58:10 +0800

    验证的性质PropertyVerified

---
+++ sbid/_M/ResultShow/PropertyVerified.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sbid._M
+{
+    // 验证的性质
+    public class PropertyVerified : ReactiveObject
+    {
+        private string description;
+        private bool verified;
+        private string counterExample;
+
+        public PropertyVerified(string description, bool verified, string counterExample)
+        {
+            this.description = description;
+            this.verified = verified;
+            this.counterExample = counterExample;
+        }
+
+        public string Description { get => description; set => description = value; }
+        public bool Verified { get => verified; set => verified = value; }
+        public string CounterExample { get => counterExample; set => counterExample = value; }
+    }
+}
