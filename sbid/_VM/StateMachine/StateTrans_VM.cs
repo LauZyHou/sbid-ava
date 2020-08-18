498398b7dbe81a0731ea0dd93e5ed577aa1b34bf
unknown <java233@foxmail.com>
2020-08-18 23:59:04 +0800

    状态转移辅助界面

--- sbid/_VM/StateMachine/StateTrans_VM.cs
+++ sbid/_VM/StateMachine/StateTrans_VM.cs
@@ -77,6 +77,32 @@ namespace sbid._VM
             ResourceManager.mainWindowVM.Tips = "打开了转移关系的编辑窗体";
         }
 
+        // 删除这个StateTrans
+        public void DeleteStateTrans()
+        {
+            // 获取当前"进程模板-状态机"侧栏面板
+            ProcessToSM_P_VM processToSM_P_VM = (ProcessToSM_P_VM)ResourceManager.mainWindowVM.SelectedItem.PanelVMs[1].SelectedItem;
+            // 获取当前的状态机面板
+            StateMachine_P_VM stateMachine_P_VM = processToSM_P_VM.SelectedItem;
+            // 删除状态上所有连线，并同时维护面板的活动锚点
+            foreach (Connector_VM connector_VM in ConnectorVMs)
+            {
+                // 如果活动锚点在这个要删除的StateTrans上就会假死，所以要判断并清除活动锚点
+                if (connector_VM == stateMachine_P_VM.ActiveConnector)
+                    stateMachine_P_VM.ActiveConnector = null;
+                // 清除该锚点上的连线，这里直接调用这个方法，即和用户手动点掉连线共享一样的行为
+                if (connector_VM.ConnectionVM != null)
+                {
+                    stateMachine_P_VM.BreakTransitionVM(connector_VM);
+                }
+            }
+
+            // 从当前状态机面板删除这个StateTrans
+            stateMachine_P_VM.UserControlVMs.Remove(this);
+
+            ResourceManager.mainWindowVM.Tips = "已经删除状态转移";
+        }
+
         #endregion
 
         // 刷新锚点位置
